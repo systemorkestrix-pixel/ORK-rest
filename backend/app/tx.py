ae459b2813a520c6619a085dcf5b7ff27ef01cb7
systemorkestrix-pixel <systemorkestrix@gmail.com>
2026-04-24 16:40:33 +0100

    Add files via upload

--- backend/app/tx.py
+++ backend/app/tx.py
@@ -6,10 +6,20 @@ from sqlalchemy.orm import Session
 
 @contextmanager
 def transaction_scope(db: Session) -> Iterator[None]:
-    if db.in_transaction():
+    if db.in_nested_transaction():
         with db.begin_nested():
             yield
+        return
+
+    if db.in_transaction():
+        try:
+            yield
+            db.commit()
+        except Exception:
+            db.rollback()
+            raise
+        return
+
     else:
         with db.begin():
             yield
-
